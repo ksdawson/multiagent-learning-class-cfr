0ff7a3e40698614fd0b24902a934c0a225fc0138
Kameron Dawson <ksdawson@mit.edu>
2025-10-27 01:34:08 -0400

    start

---
+++ main.py
@@ -0,0 +1,17 @@
+def find_the_best_response():
+    pass
+
+def learning_to_best_respond():
+    pass
+
+def learning_the_nash_equilibrium():
+    pass
+
+def main():
+    # TODO: Load the text files
+    # TODO: Construct the game trees
+    # TODO: Plot results
+    pass
+
+if __name__ == "__main__":
+    main()
